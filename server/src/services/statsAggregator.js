131a59021c73bc2dc3426ab86836e04659407e4b
Uzair-Ahmed-Shah <msg.uzair@gmail.com>
2026-02-03 01:00:33 +0530

    feat: completed stats aggregator, added trendDict for drawing line graph

--- server/src/services/statsAggregator.js
+++ server/src/services/statsAggregator.js
@@ -29,12 +29,48 @@ const getStats = async () => {
                 sentiment : {not : null}
             }
         })
+        const data = await prisma.article.findMany({
+            where: {
+                publishedAt : {gte : thirtyDaysAgo},
+                category : {not:"AI"}
+            },
+            select : {publishedAt: true, category : true},
+            orderBy: { publishedAt: 'asc'}
+        })
+
+        const trendDict = {}
+
+        data.forEach(elem => {
+            const dateVal = elem.publishedAt.toISOString().split('T')[0]
+            if (!trendDict[dateVal]){
+                trendDict[dateVal] = {date : dateVal}
+            }
+
+            const category = article.category;
+            trendDict[dateVal][category] = (trendDict[dateVal][category] || 0) + 1
+        })
+
+        const trendData = Object.values(trendDict)
+
+        return {
+            timestamp: new Date(),
+            charts: {
+                byCategory: categoryGroups.map(elem => ({ name: elem.category, value: elem._count.category })),
+                byImpact: impactGroups.map(elem => ({ name: elem.impactType, value: elem._count.impactType })),
+                bySentiment: sentimentGroups.map(elem => ({ name: elem.sentiment, value: elem._count.sentiment })),
+                categorySentimentMatrix: catXSent
+            },
+            trends: trendHistory
+        };
 
         
 
 
 
     }catch (err) {
-
+        console.log(`Failed Stats Aggregation, error: ${err}`)
+        return {error : err}
     }
 }
+
+module.exports = {getStats}
